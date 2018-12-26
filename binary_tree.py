3c5b39a08c13395f116a372e9ae9346f8f752cce
Miracle_lms <37183438+Jokertion@users.noreply.github.com>
2018-12-26 23:58:08 +0800

    二叉树的实现

---
+++ binary_tree.py
@@ -0,0 +1,31 @@
+class Node(object):
+    def __init__(self, item):
+        self.item = item
+        self.lchild = None
+        self.rchild = None
+
+
+class Tree(object):
+    def __init__(self):
+        self.root = None
+
+    def add(self, item):
+        node = Node(item)
+        if self.root is None:
+            self.root = node
+        queue = [self.root]
+        while queue:
+            cur_node = queue.pop(0)
+            if cur_node.lchild is None:
+                cur_node.lchild = node
+                return
+            else:
+                queue.append(cur_node.lchild)
+            if cur_node.rchild is None:
+                cur_node.rchild = node
+                return
+            else:
+                queue.append(cur_node.rchild)
+
+
+tree = Tree()
